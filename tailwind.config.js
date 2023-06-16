8e64d5f5b585dd7c842aad8bff498cec3472c0fe
Dheyson Alves <dheyson.lucas@hotmail.com>
2023-06-16 16:01:23 -0300

    feat: add custom colors to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +8,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        toogle: 'rgb(var(--color-toogle) / <alpha-value>)',
+        pageBackground: 'rgb(var(--color-background) / <alpha-value>)',
+        topBackground: 'rgb(var(--color-top-background) / <alpha-value>)',
+        cardBackground: 'rgb(var(--color-card-background) / <alpha-value>)',
+        textBlue: 'rgb(var(--color-text-blue) / <alpha-value>)',
+        textPattern: 'rgb(var(--color-text-pattern) / <alpha-value>)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
